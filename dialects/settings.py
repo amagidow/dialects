92aacbc5ad13a3ae4be3947d6196f1ae49160a4f
Alexander Magidow <amagidow@gmail.com>
2022-04-13 20:07:39 -0400

    Removed django.gis from settings

--- dialects/settings.py
+++ dialects/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis',
+    #'django.contrib.gis',
     'django.contrib.postgres',
      'dialectsDB',
     'leaflet',
